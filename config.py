f9a4fac81a528eb58fd3f85ecaff1f7034722628
Om Goswami <omgoswami101@gmail.com>
2024-12-16 14:42:43 -0800

    minor filename changes

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    # Flask settings
-    SECRET_KEY = os.environ.get('SECRET_KEY', 'your_default_secret_key')
-
-    # Database settings
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL', 'sqlite:///app.db')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
