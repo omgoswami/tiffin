7b1463d521256062cb9a76ff8d7f06ce8f5ea41f
Om Goswami <omgoswami101@gmail.com>
2024-12-16 11:37:06 -0800

    added db migration and adjusted index.html and init for backend testing

--- app/__init__.py
+++ app/__init__.py
@@ -2,6 +2,10 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_marshmallow import Marshmallow
 from flask_migrate import Migrate
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
 
 db = SQLAlchemy()
 ma = Marshmallow()
@@ -9,6 +13,8 @@ migrate = Migrate()
 
 def create_app():
     app = Flask(__name__)
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     app.config.from_object('config.Config')
 
     db.init_app(app)
@@ -19,9 +25,11 @@ def create_app():
     from app.routes.user_routes import user_bp
     from app.routes.buyer_routes import buyer_bp
     from app.routes.seller_routes import seller_bp
+    from app.routes.routes import main
 
-    app.register_blueprint(user_bp, url_prefix='/api/users')
-    app.register_blueprint(buyer_bp, url_prefix='/api/buyer-profiles')
-    app.register_blueprint(seller_bp, url_prefix='/api/seller-profiles')
+    app.register_blueprint(user_bp, url_prefix='/users')
+    app.register_blueprint(buyer_bp, url_prefix='/buyers')
+    app.register_blueprint(seller_bp, url_prefix='/sellers')
+    app.register_blueprint(main)
 
     return app
