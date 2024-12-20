5cccfa916c36783caece8f84900b33e6fdb54c7e
sahil-shah0203 <sahil0203@utexas.edu>
2024-12-19 21:26:23 -0600

    frontend set up and works

--- app/static/js/App.jsx
+++
@@ -1,8 +0,0 @@
-export default function App() {
-    return (
-      <div className="text-center p-6">
-        <h1 className="text-4xl font-bold text-blue-500">Hello from React!</h1>
-      </div>
-    );
-  }
-  
